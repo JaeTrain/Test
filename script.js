98e2df2439eae28d763ebaaadebc267abe256f76
JaeTrain <jaequangardner2024@afscholars.org>
2025-05-06 09:09:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let superSaiyan=document.querySelector(".option-one");
+let fusion=document.querySelector(".option-one-screen");
+let open=document.querySelector(".story-opening");
+
+let cursed=document.querySelector(".option-two");
+  let gojo=document.querySelector(".option-two-screen");
+  
+  
+  
+superSaiyan.onclick=function(){
+  fusion.style.display="block"
+  open.style.display="none"
+};
+
+
+  
+  cursed.onclick=function(){
+    gojo.style.display="block";
+    open.style.display="none";
+  };
+  
